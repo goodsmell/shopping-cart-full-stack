a2d13a82b6c71aad0934b80f0c8741aeaf80d169
goodsmell <87801306+goodsmell@users.noreply.github.com>
2026-06-06 23:48:39 +0900

    feat: 리뷰용 장바구니 시드 데이터 추가

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import app from "./app.js";
+import { seedInitialData } from './repositories/seedData.js';
 
 const PORT = process.env.PORT ?? 3000;
+
+seedInitialData();
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
