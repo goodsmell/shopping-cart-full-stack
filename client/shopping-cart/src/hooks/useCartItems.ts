7fbafc9532ddf562d8e46be18aa13d5e1bb68688
goodsmell <87801306+goodsmell@users.noreply.github.com>
2026-06-06 15:42:50 +0900

    feat: useCartItmes에서 api를 주입받으면서 DIP 적용

--- client/shopping-cart/src/hooks/useCartItems.ts
+++ client/shopping-cart/src/hooks/useCartItems.ts
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react';
 import type { CartItem } from '../types';
-import getCartList from '../apis/getCartList';
 
-const useCartItems = () => {
+const useCartItems = (fetchCartItems: () => Promise<CartItem[]>) => {
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
@@ -10,7 +9,7 @@ const useCartItems = () => {
   useEffect(() => {
     const loadCartItems = async () => {
       try {
-        const data = await getCartList();
+        const data = await fetchCartItems();
         setCartItems(data);
       } catch (error) {
         console.error(error);
